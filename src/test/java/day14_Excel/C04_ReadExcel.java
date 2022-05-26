0620fb28845f174dcdbaf76bf269526a93b5ceab
Ayhan Yazilim <yazilimdefteri1@gmail.com>
2022-05-26 19:28:24 +0300

    26.05.2022

--- src/test/java/day14_Excel/C04_ReadExcel.java
+++ src/test/java/day14_Excel/C04_ReadExcel.java
@@ -1,15 +1,17 @@
 package day14_Excel;
 
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class C02_ReadExcel {
+public class C04_ReadExcel {
     @Test
     public void readExcelTest() throws IOException {
+
         //  1. Dosya yolunu bir String degiskene atayalim
         String dosyaYolu="src/resources/ulkeler.xlsx";
         //  2. FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
@@ -17,19 +19,17 @@ public class C02_ReadExcel {
         //  3. Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
         //  WorkbookFactory.create(fileInputStream)
         Workbook workbook= WorkbookFactory.create(fis);
-        //  4. Worksheet objesi olusturun
-           //  workbook.getSheetAt(index)
-        //  6. Row objesi olusturun sheet.getRow(index)
-       //  7. Cell objesi olusturun row.getCell(index
-        String actualData=workbook
-                                  .getSheet("Sayfa1")
-                                  .getRow(3)
-                                  .getCell(3)
-                                  .toString();
 
+        // ingilizce baskentler sutununu yazdiralim
 
-        System.out.println(actualData);
+        int sonSatir=workbook.getSheet("Sayfa1")
+                .getLastRowNum();
+        System.out.println("sonSatir = " + sonSatir);
+        String satirdakiData ="";
+        for (int i = 0; i <=sonSatir ; i++) {
+            satirdakiData = C03_ReadExcel.banaDataGetir(i,1 );
+            System.out.println(satirdakiData);
+        }
 
-        // 3.index'deki satirin 3. index'indeki datanin cezayir oldugunu test edin
     }
 }
